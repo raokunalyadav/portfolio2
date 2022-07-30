4c4feee8f7aebc66dcf32d2fe9ad6900e06da6a8
raokunalyadav <108231719+raokunalyadav@users.noreply.github.com>
2022-07-30 12:03:07 +0530

    Update settings.py

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-zx)!zoox*ji&b573xb^q!$_e6a#)a7vu$$j!#*m92+2x!qv0+x'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['18.216.196.214']
 
 
 # Application definition
